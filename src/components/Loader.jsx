480d44bcdb422612ad22a69a9215a23a13e30b68
Mangelzz <mangel.grzz@gmail.com>
2023-05-21 18:49:07 -0500

    I add a dependence for infinite scroll, show pokemons whitout a button, also I add a loader animation

---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div className='border-4 border-gray-100 border-t-yellow-400 border-r-yellow-400 rounded-[50%] h-6 w-6 col-span-2 lg:col-span-5 mx-auto animate-spin'></div>
+  )
+}
+
+export default Loader
--- src/components/Pokemons.jsx
+++ src/components/Pokemons.jsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Loader from "./Loader";
 const URL_DEFAULT = "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0";
 
 const Pokemons = () => {
   const [pokemon, setPokemon] = useState([]);
   const [nextUrl, setNextUrl] = useState("");
+  const [viewMore, setViewMore] = useState(true);
 
   const getPokemon = async (url = URL_DEFAULT) => {
     //Get data from API
@@ -19,34 +22,43 @@ const Pokemons = () => {
         return {
           id: poke.id,
           name: poke.name,
-          image: poke.sprites.other.home.front_default,
+          // If render image is too slow, use this: poke.sprites.other.dream_world.front_default
+          image:
+            poke.sprites.other.home.front_default || poke.sprites.front_default,
           type: poke.types[0].type.name,
         };
       })
     );
-    return { next, pokemons, morePokemons }
+    return { next, pokemons, morePokemons, viewMore };
   };
 
   const handleGetPokemon = async () => {
     const { next, pokemons } = await getPokemon();
-    setPokemon(pokemons)
-    setNextUrl(next)
-  }
+    setPokemon(pokemons);
+    setNextUrl(next);
+  };
 
   const morePokemons = async () => {
     const { next, pokemons } = await getPokemon(nextUrl);
-    setPokemon(prev => [...prev, ...pokemons])
-    setNextUrl(next)
-  }
-
+    setPokemon((prev) => [...prev, ...pokemons]);
+    next === null && setViewMore(false);
+    setNextUrl(next);
+  };
 
   useEffect(() => {
-    handleGetPokemon()
+    handleGetPokemon();
   }, []);
   // return { pokemon, morePokemons };
 
   return (
-    <section className="grid lg:grid-cols-5 grid-cols-2 gap-4 p-3 max-w-[1200px] mx-auto mt-8">
+    <InfiniteScroll
+      dataLength={pokemon.length}
+      next={morePokemons}
+      hasMore={viewMore}
+      loader={<Loader />}
+      endMessage={<h3 className="text-3xl font-normal text-center col-span-2 lg:col-span-5 mt-6">Sorry, there are not more pokemons</h3>}
+      className="grid lg:grid-cols-5 grid-cols-2 gap-4 p-3 max-w-[1200px] mx-auto mt-8"
+    >
       {pokemon.map((pokemon) => {
         return (
           <div
@@ -61,16 +73,55 @@ const Pokemons = () => {
             <p className="flex justify-between items-center bg-gray-500 text-fuchsia-200 font-bold py-1 px-2 rounded-xl shadow-sm w-28 shadow-gray-600 mt-2 capitalize flex-col">
               <span className="text-gray-100 font-bold">#{pokemon.id}</span>
               <span className="text-gray-100 font-bold">{pokemon.name}</span>
-              <span className={`text-gray-600 p-1 rounded-lg ${
-                pokemon.type === "grass" ? "bg-green-400" : "" || pokemon.type === "fire" ? "bg-red-400" : "" || pokemon.type === "water" ? "bg-blue-400" : "" || pokemon.type === "bug" ? "bg-green-600" : "" || pokemon.type === "normal" ? "bg-gray-400" : "" || pokemon.type === "poison" ? "bg-purple-400" : "" || pokemon.type === "electric" ? "bg-yellow-400" : "" || pokemon.type === "ground" ? "bg-yellow-600" : "" || pokemon.type === "fairy" ? "bg-pink-400" : "" || pokemon.type === "fighting" ? "bg-red-600" : "" || pokemon.type === "psychic" ? "bg-pink-600" : "" || pokemon.type === "rock" ? "bg-gray-600" : "" || pokemon.type === "ghost" ? "bg-purple-600" : "" || pokemon.type === "ice" ? "bg-blue-600" : "" || pokemon.type === "dragon" ? "bg-blue-800" : "" || pokemon.type === "dark" ? "bg-gray-800" : "" || pokemon.type === "steel" ? "bg-gray-500" : "" || pokemon.type === "flying" ? "bg-blue-300" : ""
-              }`} >{pokemon.type}</span>
+              <span
+                className={`text-gray-600 p-1 rounded-lg ${
+                  pokemon.type === "grass"
+                    ? "bg-green-400"
+                    : "" || pokemon.type === "fire"
+                    ? "bg-red-400"
+                    : "" || pokemon.type === "water"
+                    ? "bg-blue-400"
+                    : "" || pokemon.type === "bug"
+                    ? "bg-green-600"
+                    : "" || pokemon.type === "normal"
+                    ? "bg-gray-400"
+                    : "" || pokemon.type === "poison"
+                    ? "bg-purple-400"
+                    : "" || pokemon.type === "electric"
+                    ? "bg-yellow-400"
+                    : "" || pokemon.type === "ground"
+                    ? "bg-yellow-600"
+                    : "" || pokemon.type === "fairy"
+                    ? "bg-pink-400"
+                    : "" || pokemon.type === "fighting"
+                    ? "bg-red-600"
+                    : "" || pokemon.type === "psychic"
+                    ? "bg-pink-600"
+                    : "" || pokemon.type === "rock"
+                    ? "bg-gray-600"
+                    : "" || pokemon.type === "ghost"
+                    ? "bg-purple-600"
+                    : "" || pokemon.type === "ice"
+                    ? "bg-blue-600"
+                    : "" || pokemon.type === "dragon"
+                    ? "bg-blue-800"
+                    : "" || pokemon.type === "dark"
+                    ? "bg-gray-800"
+                    : "" || pokemon.type === "steel"
+                    ? "bg-gray-500"
+                    : "" || pokemon.type === "flying"
+                    ? "bg-blue-300"
+                    : ""
+                }`}
+              >
+                {pokemon.type}
+              </span>
               {/* change type color depending of pokemon type */}
             </p>
           </div>
         );
       })}
-      <button onClick={morePokemons}>View more pokemons</button>
-    </section>
+    </InfiniteScroll>
   );
 };
 
